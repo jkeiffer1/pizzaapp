40c8828796ba1437f3f029cdeeaa60da49a7cb30
jkeiffer1 <saros82@hotmail.com>
2017-08-21 15:49:33 -0400

    made confirmation page/view, verified that sessions carry over to confirm view

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 require "sinatra"
+enable :sessions
+
 
 get '/' do
 	erb :pizza
 end
 
 post '/pizza' do
+	session[:size] = params[:size]
+	session[:crust] = params[:crust]
+	session[:sauce] = params[:sauce]
+	session[:meats] = params[:meats]
+	session[:veggies] = params[:veggies]
+	session[:cheese] = params[:cheese]
+	redirect "/confirm"
+end
+
+get '/confirm' do
+	erb :confirm, locals:{size:session[:size], crust:session[:crust], sauce:session[:sauce], meats:session[:meats], veggies:session[:veggies], cheese:session[:cheese]}
+end
+
+post '/confirm' do
 end
