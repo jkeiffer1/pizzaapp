3f37be84e64e20b97471f2dacaf9b8ae2e2d2396
jkeiffer1 <saros82@hotmail.com>
2017-08-22 13:57:40 -0400

    added results page, carried sessions into results page via erb and redirect

--- app.rb
+++ app.rb
@@ -21,4 +21,17 @@ get '/confirm' do
 end
 
 post '/confirm' do
+	session[:size] = params[:size]
+	session[:crust] = params[:crust]
+	session[:sauce] = params[:sauce]
+	session[:meats] = params[:meats]
+	session[:veggies] = params[:veggies]
+	session[:cheese] = params[:cheese]
+	session[:delivery] = params[:delivery]
+	session[:address] = params[:address]
+	redirect "/results"
+end
+
+get '/results' do
+	erb :results, locals:{size:session[:size], crust:session[:crust], sauce:session[:sauce], meats:session[:meats], veggies:session[:veggies], cheese:session[:cheese], delivery:session[:deliver], address:session[:address]}
 end
